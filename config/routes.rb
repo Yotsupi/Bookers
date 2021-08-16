78050930a536c30e8b372e6d9529204b8d800325
Yotsupi <kayoshida.1830@gmail.com>
2021-08-16 09:45:51 +0000

    [Add]updateアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/books' => 'books#index'
   post '/books' => 'books#create'
   get '/books/:id' => 'books#show',as:'book'
-  get 'books/:id/edit' => 'books#edit',as:'edit_book'
-  patch 'books/:id' => 'books#update',as:'update_book'
+  get '/books/:id/edit' => 'books#edit',as:'edit_book'
+  patch '/books/:id' => 'books#update',as:'update_book'
 end
